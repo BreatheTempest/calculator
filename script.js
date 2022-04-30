c8c47053dfa54ddbf0a1a02aecd18008f4e525fc
Anton Zabulonov <zabulonov.a@gmail.com>
2022-04-30 22:51:29 +0300

    Tidy up visuals and add listeners for keys

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function operate(func, num1, num2) {
 	return func(num1, num2);
 }
 console.log(operate(add, 4, 5));
+
+const nums = document.querySelectorAll('.num');
+const display = document.querySelector('.display');
+
+let firstNum;
+let operation;
+let secondNum;
+
+nums.forEach((num) =>
+	num.addEventListener('click', () => {
+		firstNum = num.textContent;
+		display.textContent += num.textContent;
+	})
+);
